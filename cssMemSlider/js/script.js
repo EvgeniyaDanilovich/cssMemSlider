7cd95eedd2ca901e599eb52ca3bbda97832c1c10
EvgeniyaDanilovich <evgeniyadanilovich@gmail.com>
2022-05-11 16:11:25 +0300

    feat: implement animation (Wed, May 11, 2022 16:10:00 PM)

--- cssMemSlider/js/script.js
+++ cssMemSlider/js/script.js
@@ -4,26 +4,49 @@ const dots = document.querySelectorAll('.controls__dot');
 const texts = document.querySelectorAll('.signature__text');
 
 let count = 0;
+let prevCount = 0;
 
-const remove = () => {
-	carouselItems.forEach(img => {
-		img.classList.remove('active');
+function remove(direction) {
+	carouselItems[prevCount].classList.add(direction);
+	carouselItems[prevCount].addEventListener('animationend', () => {
+		carouselItems[prevCount].classList.remove('active', direction);
 	})
-	texts.forEach(text => {
-		text.classList.remove('active');
+
+	texts[prevCount].classList.add('remove-text');
+	texts[prevCount].addEventListener('animationend', () => {
+		texts[prevCount].classList.remove('remove-text', 'active');
 	})
 }
 
-const addActive = (count) => {
-	carouselItems[count].classList.add('active');
-	texts[count].classList.add('active');
+function addActive(direction) {
+	carouselItems[count].classList.add('next', direction);
+	carouselItems[count].addEventListener('animationend', () => {
+		carouselItems[count].classList.add('active');
+		carouselItems[count].classList.remove('next', direction);
+	});
+
+	texts[count].classList.add('next', 'show-text');
+	texts[count].addEventListener('animationend', () => {
+		texts[count].classList.add('active');
+		texts[count].classList.remove('next', 'show-text');
+	});
 }
 
-dots.forEach((dot, i) => {
-	dot.addEventListener('click', () => {
+dots.forEach(function (dot, i) {
+	dot.addEventListener('click', function () {
+		prevCount = count;
 		count = i;
-		remove();
-		addActive(count);
+
+		if (prevCount > count) {
+			remove('to-right');
+			addActive('from-left');
+		} else if (prevCount === count) {
+			return
+		} else {
+			remove('to-left');
+			addActive('from-right');
+		}
+
 	})
 })
 
